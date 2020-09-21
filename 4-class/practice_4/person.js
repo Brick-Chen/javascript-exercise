89ce103475f73a9d7b7b2849b5a62d783e1c63be
YumingChen <mathcym@outlook.com>
2020-09-21 18:31:16 +0800

    [yuming chen] finish 4-class pratice_4.

--- 4-class/practice_4/person.js
+++ 4-class/practice_4/person.js
@@ -1,1 +1,17 @@
-// TODO 15: 在这里写实现代码
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    return this.basicIntro();
+  }
+
+  basicIntro() {
+    const outName = this.name;
+    const outAge = this.age;
+    const intro = `My name is ${outName}. I am ${outAge} years old.`;
+    return intro;
+  }
+}
