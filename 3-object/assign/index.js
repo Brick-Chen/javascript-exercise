814aee49a87b000664333f1932e15cda39de9e54
YumingChen <mathcym@outlook.com>
2020-09-21 16:00:40 +0800

    [yuming chen]: finish 3-object.

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,7 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const processedObj = Object.assign(source);
+  processedObj.properties.status = 'processed';
+  processedObj.serialNumber = '12345';
+  return processedObj;
 }
