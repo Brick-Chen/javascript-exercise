d6131d58bf4a5b3dff86d6bc71222e88aaa4bb34
YumingChen <mathcym@outlook.com>
2020-09-21 18:49:38 +0800

    [yuming chen]: finish 4-class practice_5.

--- 4-class/practice_5/student.js
+++ 4-class/practice_5/student.js
@@ -1,1 +1,15 @@
-// TODO 19: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    const outKclass = this.klass;
+    const basicInfo = super.introduce();
+    const intro = ` I am a Student. I am at Class ${outKclass}.`;
+    return basicInfo + intro;
+  }
+}
--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
-// TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass = '') {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    const basicInfo = super.introduce();
+    const classInfo = this.klass === '' ? 'No Class.' : `Class ${this.klass}.`;
+    const teacherInfo = ' I am a Teacher. I teach ';
+    return basicInfo + teacherInfo + classInfo;
+  }
+}
